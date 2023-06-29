c78d3808ba4cd59914ccac3b64edd9570b592487
usmanasgh <usmanasgh@live.com>
2023-06-29 20:44:06 +0100

    Bug fixes

--- Cars/Cars.UI/Models/ResponseDTO.cs
+++ Cars/Cars.UI/Models/ResponseDTO.cs
@@ -3,7 +3,7 @@
     public class ResponseDTO
     {
         public object? Result { get; set; }
-        public bool Success { get; set; } = true; // Default value
+        public bool Success { get; set; } = false; // Default value
         public string Message { get; set; } = string.Empty; // Default empty string
 
     }
